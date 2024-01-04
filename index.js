ce2ccc0d59aeace03c8eaf80882744c7a094a022
MennuaD <114007110+MennuaD@users.noreply.github.com>
2024-01-04 15:33:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("I will create a server using Express module");
+const express = require('express');
+const app = express();
+app.listen(5500, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+//this serves HTML  web pages
+//this helps serve the web page stored in this folder at localhost:3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
